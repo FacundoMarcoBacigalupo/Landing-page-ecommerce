cce9097630cc8f3699bd7c55ac2f2e3a0b44a98f
Facundo Marco Bacigalupo <marcofacundolucas@gmail.com>
2023-04-17 23:28:51 -0300

    tercera pre entrega

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-//Primero se le da la bienvenida al cliente, luego se le pregunta si está interesado en nuestros Servicios, después se le pregunta en cuál de los servicios tiene interés y se termina.
-
+//Primero se le da la bienvenida al cliente, luego se le pregunta si está interesado en nuestros Servicios, después se le pregunta en cuál de los servicios tiene interés y al final se pregunta si quiere saber nuevamente los nombres y precios de los servicios.
 
 alert("Bienvenido a MFL")
 
-    const servicio1 = " Jupiter"
-    const servicio2 = " Saturno"
-    const servicio3 = " Urano"
-
-    const serviciosMFl = [servicio1,servicio2,servicio3]
-
+                            //Nombre de todos los servicios de MFL
+    const servicio1 = "Júpiter"
+    const servicio2 = "Saturno"
+    const servicio3 = "Urano"
 
 
 
+                            //Array con todos los servicios de MFL(serviciosMFl)
+    const serviciosMFl = [
+        {nombre: servicio1, contenido: "4 publicaciones", precio: 9000},
+        {nombre: servicio2, contenido: "8 publicaciones" , precio: 11000},
+        {nombre: servicio3, contenido: "12 publicaciones", precio: 15000}
+    ]
 
-                                            //Funcion constructura
+
+
+
+
+                            //Nombres de mis servicios dentro de mi Array (serviciosMFl)
+    const serviciosNombres = serviciosMFl.map((nombres) => nombres.nombre).join(", ");
+
+
+
+
+
+
+                            //Función constructura
     function Servicio(nombre, contenido, precio){
 
             this.nombre = nombre
             this.contenido = contenido
             this.precio = precio
 
-            this.info = function(){
-                let respuestaCliente = prompt(`Este servicio se llama ${nombre}, tiene un valor de ${precio}, cuenta con ${contenido}, ¿Desea continuar con este servicio? SI-NO`).toUpperCase().trim( )
+            this.info = ()=>{
+                let respuestaCliente = prompt(`Este servicio se llama ${nombre}, cuenta con ${contenido} y tiene un valor de ${precio} ¿Desea continuar con este servicio? SI-NO`).toUpperCase().trim()
 
                 if (respuestaCliente === true || respuestaCliente === "SI"){
                     alert ("+54 11 2904439 este es nuestro numero de celular para contactarnos y concreatar una reunion, lo esperamos.")
@@ -36,38 +51,35 @@ alert("Bienvenido a MFL")
 
 
 
-
-
-                                            //Servicio JUPITER
-    const jupiter = new Servicio("Jupiter", "4 publicaciones", "$9000 ARS")
-
+                            //Servicio JÚPITER
+    const jupiter = new Servicio(servicio1, "4 publicaciones", "$9000 ARS")
 
 
 
-
-                                            //Servicio SATURNO
-    const saturno = new Servicio("Saturno", "8 publicaciones", "$11000 ARS")
-
+                            //Servicio SATURNO
+    const saturno = new Servicio(servicio2, "8 publicaciones", "$11000 ARS")
 
 
 
-
-                                            //Servicio URANO                        
-    const urano = new Servicio("Urano", "12 publicaciones", "$15000 ARS")
+                            //Servicio URANO                        
+    const urano = new Servicio(servicio3, "12 publicaciones", "$15000 ARS")
 
 
 
 
 
-let consulta = confirm("¿Estas interesado en nuestros servicios de Marketing Digital? Contamos con estos 3 servicios:" + serviciosMFl)
+
+
+let consulta = confirm(`¿Estas interesado en nuestros servicios de Marketing Digital? Contamos con estos servicios: ${serviciosNombres}`)
+
 
 
 
 if(consulta === true){
 
-    let interesCliente = prompt("¿Que servicio le interesa de los que tenemos?").toLowerCase().trim( )
+    let interesCliente = prompt("¿Que servicio le interesa de los que tenemos?").toLowerCase().trim()
 
-    while(interesCliente !=" " && interesCliente != null){
+    while(interesCliente !="" && interesCliente !=null){
 
         if(interesCliente === "jupiter"){
             alert(jupiter.info())
@@ -85,13 +97,28 @@ if(consulta === true){
         }
 
         else{
-            alert("No contamos con el servicio " + interesCliente + ", intente con uno de estos sercicios: " + serviciosMFl)
+            alert(`No contamos con el servicio ${interesCliente} intente con uno de estos sercicios: ${serviciosNombres}`)
             break;
         }
     }
-
 
 }
 else{
     alert("Esperamos volver a verte por acá")
 }
+
+
+
+
+
+let consultaPrecios = prompt(`¿Queres conocer todos los nombres y precios de los servicios que tenemos?`).toUpperCase().trim()
+
+
+    if(consultaPrecios === true || consultaPrecios === "SI"){
+        for(let i = 0; i < serviciosMFl.length;i++){
+        alert( ( serviciosMFl[i].nombre + ", " + "$".concat(serviciosMFl[i].precio + " ARS") ) )
+        }
+    }
+    else{
+        alert("Esperamos volver a verte por acá")
+    }
